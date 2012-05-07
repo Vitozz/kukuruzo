e79f5829769212d3a964c9ec0e7827442594145f
Vitaly Tonkacheyev <thetvg@gmail.com>
2012-05-07 17:21:17 +0300

    New release of RegExpTest project

--- qt/regexptest/mainwindow.cpp
+++ qt/regexptest/mainwindow.cpp
@@ -2,19 +2,33 @@
 #include "ui_mainwindow.h"
 #include "stdlib.h"
 
-#include "QFile"
-#include "QTextStream"
-#include "QIODevice"
-#include "QRegExp"
-#include "QMessageBox"
-#include "QDesktopServices"
-#include "QUrl"
-#include "QFileDialog"
-#include "QDir"
-#include "QSettings"
+#include <QFile>
+#include <QTextStream>
+#include <QIODevice>
+#include <QRegExp>
+#include <QMessageBox>
+#include <QDesktopServices>
+#include <QUrl>
+#include <QFileDialog>
+#include <QDir>
+#include <QSettings>
+
+#define TEXT_OPT "text"
+#define FILENAME_OPT "filename"
+#define REGEXP_OPT "regexp"
+#define ISQTSTYLE_OPT "qtstyle"
+#define ISW3C_OPT "isw3c"
+#define ISCASE_OPT "iscase"
+#define ISREGEXP_OPT "isregexp"
+#define ISREGEXP2_OPT "isregexp2"
+#define ISWILDCARD_OPT "iswildcard"
+#define ISWILDCARDLUNIX_OPT "iswildcardlinux"
+#define ISEXACT_OPT "isexact"
+#define ISMINIMAL_OPT "isminimal"
 
 static const int CHECK_ALL = 1;
 static const int CHECK_POS = 2;
+
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
@@ -50,14 +64,13 @@ void MainWindow::on_runit_clicked()
 {
 	QList <QStringList> matches = CheckExpression(regExpText_,inputText_);
 	ui->label->document()->setHtml(GetRegexpList(matches,CHECK_ALL,0));
-
 }
 
 void MainWindow::readSettings(){
 	QSettings settings;
-	inputText_ = settings.value("text", "").toString();
-	fileName_ = settings.value("filename", "").toString();
-	regExpText_ = settings.value("regexp", "").toString();
+	inputText_ = settings.value(TEXT_OPT, "").toString();
+	fileName_ = settings.value(FILENAME_OPT, "").toString();
+	regExpText_ = settings.value(REGEXP_OPT, "").toString();
 	if(!regExpText_.isEmpty()&&(!regExpText_.isNull())){
 		ui->RegExp->setText(regExpText_);
 	}
@@ -66,37 +79,57 @@ void MainWindow::readSettings(){
 		inputText_ = LoadTextFile(fileName_);
 		ui->inputText->setPlainText(inputText_);
 		dir_ = getDirName(fileName_) + "/";
-	}else{
+	}
+	else{
 		if(!inputText_.isEmpty()&&(!inputText_.isNull())){
 			ui->inputText->setPlainText(inputText_);
 		}
 	}
+	ui->unquotebox->setChecked(settings.value(ISQTSTYLE_OPT, false).toBool());
+	ui->iscase->setChecked(settings.value(ISCASE_OPT, false).toBool());
+	ui->isexactmatch->setChecked(settings.value(ISEXACT_OPT, false).toBool());
+	ui->isminimal->setChecked(settings.value(ISMINIMAL_OPT, false).toBool());
+	ui->isregexp->setChecked(settings.value(ISREGEXP_OPT, false).toBool());
+	ui->isregexp2->setChecked(settings.value(ISREGEXP2_OPT, false).toBool());
+	ui->isw3c->setChecked(settings.value(ISW3C_OPT, false).toBool());
+	ui->iswildcard->setChecked(settings.value(ISWILDCARD_OPT, false).toBool());
+	ui->iswildcardunix->setChecked(settings.value(ISWILDCARDLUNIX_OPT, false).toBool());
 }
 
 void MainWindow::writeSettings(){
 	QSettings settings;
 	if(!fileName_.isEmpty()){
-		settings.setValue("filename", fileName_);
-		settings.setValue("text", "");
+		settings.setValue(FILENAME_OPT, QVariant(fileName_));
+		settings.setValue(TEXT_OPT, "");
 	}else{
-		settings.setValue("text", inputText_);
-		settings.setValue("filename", "");
+		settings.setValue(TEXT_OPT, QVariant(inputText_));
+		settings.setValue(FILENAME_OPT, "");
 	}
-	settings.setValue("regexp", regExpText_);
+	settings.setValue(REGEXP_OPT, QVariant(regExpText_));
+	settings.setValue(ISQTSTYLE_OPT, QVariant(ui->unquotebox->isChecked()));
+	settings.setValue(ISW3C_OPT, QVariant(ui->isw3c->isChecked()));
+	settings.setValue(ISCASE_OPT, QVariant(ui->iscase->isChecked()));
+	settings.setValue(ISREGEXP_OPT, QVariant(ui->isregexp->isChecked()));
+	settings.setValue(ISREGEXP2_OPT, QVariant(ui->isregexp2->isChecked()));
+	settings.setValue(ISWILDCARD_OPT, QVariant(ui->iswildcard->isChecked()));
+	settings.setValue(ISWILDCARDLUNIX_OPT, QVariant(ui->iswildcardunix->isChecked()));
+	settings.setValue(ISEXACT_OPT, QVariant(ui->isexactmatch->isChecked()));
+	settings.setValue(ISMINIMAL_OPT, QVariant(ui->isminimal->isChecked()));
 }
 
-QString MainWindow::GetRegexpList(const QList<QStringList> &matches, const int &parm, const int &pos){
+QString MainWindow::GetRegexpList(const QList<QStringList> &matches, const int &parm, const int &pos) const
+{
 	QString result;
-	QStringList group;
 	if(!matches.isEmpty()){
-		foreach(group,matches){
+		foreach(QStringList group, matches) {
 			result += QString("<u><a style=\"color:red\">Match # </a>") + "<b>"+ QString::number(matches.indexOf(group)+1) + "</b></u><br>";
 			if(parm == CHECK_ALL){
 				for(int i=0; i<group.length();i++){
 					result += QString("<a style=\"color:blue\">Group # </a>")+ "<b>"+QString::number(i)+ ": </b>"+"<i>" + Qt::escape(group[i]) + "</i><br>";
 				}
-			}else if(parm == CHECK_POS){
-				if(pos < group.length()){
+			}
+			else if(parm == CHECK_POS) {
+				if(pos < group.length()) {
 					result += QString("<a style=\"color:blue\">Group # </a>")+ "<b>"+ QString::number(pos)+ ": </b>"+"<i>" + Qt::escape(group[pos]) + "</i><br>";
 				}
 			}
@@ -105,23 +138,22 @@ QString MainWindow::GetRegexpList(const QList<QStringList> &matches, const int &
 	return result;
 }
 
-QList <QStringList> MainWindow::CheckExpression(const QString &regexp, const QString &text){
+QList <QStringList> MainWindow::CheckExpression(const QString &regexp, const QString &text) const{
 	QRegExp rx(regexp, sens_, pattern_);
 	QList <QStringList> result;
-	if(rx.isValid()){
-		if(ui->isminimal->isChecked()){
+	if(rx.isValid() && !rx.isEmpty()) {
+		if(ui->isminimal->isChecked()) {
 			rx.setMinimal(true);
-		}else{
+		}
+		else {
 			rx.setMinimal(false);
 		}
 		int pos = rx.indexIn(text);
-		///qDebug("Pos %d", pos);
 		while(((pos = rx.indexIn(text, pos)) != -1)&&(pos < text.length())){
-			//qDebug("Pos %d", pos);
-			ui->spinBox->setMaximum(rx.captureCount());
 			result << rx.capturedTexts();
 			pos += rx.matchedLength();
 		}
+		ui->spinBox->setMaximum(rx.captureCount());
 	}
 	return result;
 }
@@ -139,10 +171,10 @@ QString MainWindow::LoadTextFile(const QString &filename){
 		QTextStream in(&file);
 		if(file.open(QIODevice::ReadOnly)){
 			result = in.readAll();
-			isLoaded = true;
+			isLoaded_ = true;
 		}
 	}else{
-		isLoaded = false;
+		isLoaded_ = false;
 	}
 	return result;
 }
@@ -151,7 +183,7 @@ void MainWindow::RunOnlineHelp()
 {
 	QString helpdir;
 #ifdef Q_OS_WIN32
-	helpdir = "/" + qApp->applicationDirPath() + "/docs";
+	helpdir = qApp->applicationDirPath() + "/docs";
 #endif
 #ifdef Q_OS_LINUX
 	helpdir = "/usr/share/doc/regexptest/html";
@@ -212,11 +244,11 @@ void MainWindow::on_inputText_textChanged()
 {
 	if(!ui->inputText->document()->isEmpty()){
 		inputText_ = ui->inputText->document()->toPlainText();
-		if(!isLoaded){
+		if(!isLoaded_){
 			ui->fileName->setText("");
 			fileName_ = "";
 		}else{
-			isLoaded = false;
+			isLoaded_ = false;
 		}
 	}
 }
@@ -253,7 +285,7 @@ void MainWindow::on_openFile_clicked()
 	}
 }
 
-QString MainWindow::getDirName(const QString &filename){
+QString MainWindow::getDirName(const QString &filename) const{
 	QString result;
 	QRegExp dirregexp("(.*)/(?:.*|\\..*)$");
 	if (dirregexp.isValid()){
@@ -265,20 +297,46 @@ QString MainWindow::getDirName(const QString &filename){
 	return result;
 }
 
-QString MainWindow::replaceNotGreedy(QString text){
-	if(!text.isEmpty()&&(!text.isNull())){
+QString MainWindow::replaceNotGreedy(QString text) const{
+	if(!text.isEmpty()){
 		text.replace("+?","+");
 		text.replace("*?","*");
 		text.replace("}?","}");
-		return text;
 	}
-	return "";
+	return text;
+}
+
+QString MainWindow::unquoteText(QString &text) const
+{
+	if (!text.isEmpty()) {
+		text.replace("\\\\", "\\");
+	}
+	return text;
+}
+
+QString MainWindow::quoteText(QString &text) const
+{
+	if (!text.isEmpty()) {
+		text.replace("\\", "\\\\");
+	}
+	return text;
 }
 
 void MainWindow::on_RegExp_textChanged(QString )
 {
 	if(!ui->RegExp->text().isEmpty()){
-		QString tmp = ui->RegExp->text();
-		regExpText_ = replaceNotGreedy(tmp);
+		originRegExpText_ = ui->RegExp->text();
+		QString tmp = replaceNotGreedy(originRegExpText_);
+		regExpText_ = ui->unquotebox->isChecked() ? unquoteText(tmp) : tmp;
+	}
+}
+
+void MainWindow::on_unquotebox_toggled(bool checked)
+{
+	if (checked) {
+		ui->RegExp->setText(quoteText(originRegExpText_));
+	}
+	else {
+		ui->RegExp->setText(unquoteText(originRegExpText_));
 	}
 }
--- qt/regexptest/mainwindow.h
+++ qt/regexptest/mainwindow.h
@@ -5,52 +5,56 @@
 #include <QRegExp>
 
 namespace Ui {
-    class MainWindow;
+class MainWindow;
 }
 
 class MainWindow : public QMainWindow {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+	MainWindow(QWidget *parent = 0);
+	~MainWindow();
 
 protected:
-    void changeEvent(QEvent *e);
-    void closeEvent(QCloseEvent *e);
+	void changeEvent(QEvent *e);
+	void closeEvent(QCloseEvent *e);
 
 private:
-    Ui::MainWindow *ui;
-    void RunOnlineHelp();
-    QString LoadTextFile(const QString &filename);
-    QList <QStringList> CheckExpression(const QString &regexp, const QString &text);
-    QString GetRegexpList(const QList <QStringList> &matches, const int &parm, const int &pos);
-    void readSettings();
-    void writeSettings();
-    QString replaceNotGreedy(QString text);
-    QString getDirName(const QString &filename);
+	Ui::MainWindow *ui;
+	void RunOnlineHelp();
+	QString LoadTextFile(const QString &filename);
+	QList <QStringList> CheckExpression(const QString &regexp, const QString &text) const;
+	QString GetRegexpList(const QList <QStringList> &matches, const int &parm, const int &pos) const;
+	void readSettings();
+	void writeSettings();
+	QString replaceNotGreedy(QString text) const;
+	QString unquoteText(QString &text) const;
+	QString quoteText(QString &text) const;
+	QString getDirName(const QString &filename) const;
 
-    Qt::CaseSensitivity sens_;
-    QRegExp::PatternSyntax pattern_;
-    QString fileName_;
-    QString inputText_;
-    QString regExpText_;
-    QString dir_;
-    bool isLoaded;
+	Qt::CaseSensitivity sens_;
+	QRegExp::PatternSyntax pattern_;
+	QString fileName_;
+	QString inputText_;
+	QString regExpText_;
+	QString originRegExpText_;
+	QString dir_;
+	bool isLoaded_;
 
 private slots:
-    void on_RegExp_textChanged(QString );
-    void on_openFile_clicked();
-    void on_spinBox_valueChanged(int );
-    void on_runit_clicked();
-    void on_actionOnline_Help_activated();
-    void on_inputText_textChanged();
-    void on_fileName_textChanged(QString );
-    void on_iswildcardunix_toggled(bool checked);
-    void on_iswildcard_toggled(bool checked);
-    void on_isregexp2_toggled(bool checked);
-    void on_isregexp_toggled(bool checked);
-    void on_iscase_toggled(bool checked);
-    void on_isw3c_toggled(bool checked);
+	void on_RegExp_textChanged(QString );
+	void on_openFile_clicked();
+	void on_spinBox_valueChanged(int );
+	void on_runit_clicked();
+	void on_actionOnline_Help_activated();
+	void on_inputText_textChanged();
+	void on_fileName_textChanged(QString );
+	void on_iswildcardunix_toggled(bool checked);
+	void on_iswildcard_toggled(bool checked);
+	void on_isregexp2_toggled(bool checked);
+	void on_isregexp_toggled(bool checked);
+	void on_iscase_toggled(bool checked);
+	void on_isw3c_toggled(bool checked);
+	void on_unquotebox_toggled(bool checked);
 };
 
 #endif // MAINWINDOW_H
