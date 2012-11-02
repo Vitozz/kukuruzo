ffbbf3c7df3be48da152c001523648c55eaea103
Vitaly Tonkacheyev <thetvg@gmail.com>
2012-11-03 00:06:19 +0200

    pyInterpolation now using a parametric equation of the line

--- python/pyinterpol/pyInterpol/pyInterpol.py
+++ python/pyinterpol/pyInterpol/pyInterpol.py
@@ -6,8 +6,14 @@ pygtk.require('2.0')
 import gtk
 import gtk.glade
 
+from .reswork import loadResFile
+
+PROJECT="pyinterpol"
+VERSION="0.3"
+
 class Interpol():
 	def __init__(self):
+		self.loader = loadResFile()
 		self.way = None
 		self.X1 = 0
 		self.X2 = 0
@@ -18,7 +24,7 @@ class Interpol():
 		if str(os.sys.platform) == "win32":
 			self.gladefile = 'mForm.glade'
 		else:
-			self.gladefile = '/usr/share/pyinterpol/mForm.glade'
+			self.gladefile = self.loader.get(PROJECT, "mForm.glade")
 		if self.gladefile:
 			self.widgetTree = gtk.glade.XML(self.gladefile)
 		else:
@@ -39,7 +45,7 @@ class Interpol():
 	def OnAbout(self, widget):
 		about = gtk.AboutDialog()
 		about.set_program_name("pyInterpolation")
-		about.set_version("0.2")
+		about.set_version(VERSION)
 		about.set_copyright("2012(c) Vitaly Tonkacheyev (thetvg@gmail.com)")
 		about.set_comments("Программа расчета линейной интерполяции (экстраполяции)")
 		about.set_website("http://sites.google.com/site/thesomeprojects/")
@@ -68,48 +74,10 @@ class Interpol():
 		self.Interpolation()
 
 	def Interpolation(self):
-		args = (self.X1, self.X2, self.Xtr, self.Y1, self.Y2)
 		answ = 0
-		calcType = self.getCalcType(args)
-		if calcType:
-			answ = self.Interpolate(args)
-		elif calcType != None:
-			answ = self.Extrapolate(args)
+		if abs(self.X2 - self.X1) > 0:
+			answ = (self.Xtr - self.X1)*(self.Y2 - self.Y1)/(self.X2 - self.X1) + self.Y1
 		self.Yisk.set_text(str(answ))
-
-	def getCalcType(self, args):
-		if args[2] < args[1] and (args[2] > args[0]):
-			return True
-		elif args[2] > args[1] or (args[2] < args[0]):
-			return False
-		return None
-
-	def Interpolate(self, args, tg=0, c=0, d=0):
-		if abs(args[0] - args[1])>0:
-			tg = abs(args[3]-args[4])/abs(args[0]-args[1])
-			c = args[2] - min(args[0], args[1])
-			d = c*tg;
-			return (min(args[3], args[4]) + d);
-		return 0
-
-	def Extrapolate(self, args, tg=0, c=0, d=0):
-		if abs(args[0]-args[1])>0:
-			tg = abs(args[3]-args[4])/abs(args[0]-args[1])
-			if args[2] > max(args[0], args[1]):
-				c = args[2] - max(args[0], args[1])
-				d = c*tg
-				if args[4] > args[3]:
-					return (max(args[3], args[4]) + d)
-			elif args[2] < min(args[0], args[1]) :
-				c = min(args[0], args[1]) - args[2]
-				d = c*tg
-				if args[4] > args[3]:
-					return (min(args[3], args[4]) - d)
-				else:
-					return (max(args[3], args[4]) + d)
-			else:
-				return (min(args[3], args[4]) - d)
-		return 0
 
 	def OnDestroy(self, widget):
 		gtk.main_quit()
