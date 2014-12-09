78f827e6db1daa7eaf7d2a148afe680f7a32b6d7
Vitaly Tonkacheyev <thetvg@gmail.com>
2014-12-09 10:02:36 +0200

    Added Necrologue_rus.sh to run Penumbra: Necrologue mod for Amnesia

---
+++ scripts/Necrologue_rus.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Run script for Penumbra: Necrologue mod
+FCNAME="Necrologue"
+#Change this with your path
+steam_apps=${HOME}/.steam/steam/SteamApps
+#
+game_acf=$(grep -l "Amnesia: The Dark Descent" ${steam_apps}/*.acf)
+appid=$(cat ${game_acf} | grep appid | cut -d '"' -f4)
+echo "AppID = ${appid}"
+inst_dir_suffix=$(cat ${game_acf} | grep installdir | cut -d '"' -f4)
+inst_dir="${steam_apps}/common/${inst_dir_suffix}"
+echo "PATH = ${inst_dir}"
+mod_path=Necrologue_rus
+mod_main_init="${mod_path}/config/main_init.cfg"
+ARCH=$(arch)
+
+run_with_steam ()
+{
+	steam -applaunch ${appid} ${mod_main_init}
+}
+
+run_with_binary ()
+{
+	# 32-bit operating systems
+	if [ "${ARCH}" != "x86_64" -a -e "${inst_dir}/Amnesia.bin.x86" ]; then 
+		"${inst_dir}"/Amnesia.bin.x86 ${mod_main_init}
+	else
+		# 64-bit operating systems
+		if [ -e "${inst_dir}/Amnesia.bin.x86_64" ]; then
+			"${inst_dir}"/Amnesia.bin.x86_64 ${mod_main_init}
+		else
+			echo "Could not find game binary.";
+		fi
+	fi
+}
+
+run_with_steam || run_with_binary
